54cab44d6eb5b44f7129ced076821be397d61c3b
pluapp <76228426+pluapp@users.noreply.github.com>
2020-12-18 18:09:44 -0500

    Add files via upload

---
+++ script/passcode.js
@@ -0,0 +1,27 @@
+$(document).ready(function(){
+    //displaying modal on load
+    $(".password-modal").addClass("display-flex")
+    $("#pass").focus()
+    $(".password-modal").on("contextmenu",function(){
+       return false;
+    }); 
+    $("#search-box").prop('disabled', true);
+    $("body").css("overflow","hidden")
+
+    //hiding modal on passcode match
+    $(".password-modal #pass").on("keyup", function() {
+        if( $("#pass").val() === "9090" ) {
+            $(".password-modal").removeClass("display-flex")
+            $("body").css("overflow","scroll")
+            $("#search-box").prop('disabled', false);
+            $("#search-box").focus()
+        }else{
+            if($("#pass").val().length === 4){  
+                $(".password-modal #pass").addClass("shaky-shaky")
+            }
+            if( $("#pass").val() === "" ){
+                $(".password-modal #pass").removeClass("shaky-shaky")
+            }
+        }
+    })
+})
